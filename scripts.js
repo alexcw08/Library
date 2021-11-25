e5c9d04704f0224062ef62afde66bbbda362ceb6
Alex C <calex781@gmail.com>
2021-11-24 21:34:18 -0500

    Changed function declaration from constructor to the prototype.

--- scripts.js
+++ scripts.js
@@ -3,11 +3,20 @@ function Book(title, author, pages, isRead = false) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-
-  this.info = () => {
-    return `The ${title} is a book by ${author} and it has ${pages} pages. It is ${isRead}.`;
-  };
 }
+Book.prototype.info = function () {
+  return (
+    "The " +
+    this.title +
+    " is a book by " +
+    this.author +
+    " and it has " +
+    this.pages +
+    " pages. It is " +
+    this.isRead +
+    "."
+  );
+};
 const badBook = new Book("Harry Potter", "Jameson", 650, "not read");
 console.log(badBook);
 console.log(badBook.info());
